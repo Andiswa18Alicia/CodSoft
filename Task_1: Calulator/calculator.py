48ea93879176510665f4f1fbe6a284afbd53488b
anngcon022 <anngcon022@student.wethinkcode.co.za>
2023-10-30 11:15:59 +0200

    Simple Calculator

--- Task_1: Calulator/calculator.py
+++ Task_1: Calulator/calculator.py
@@ -1,13 +1,17 @@
 import tkinter as tk
+
 from tkinter import PhotoImage
+
 
 def add_digit(digit):
     current_text = entry_display.get()
     entry_display.delete(0, tk.END)
     entry_display.insert(0, current_text + digit)
 
+
 def clear():
     entry_display.delete(0, tk.END)
+
 
 def calculate():
     expression = entry_display.get()
@@ -19,28 +23,35 @@ def calculate():
         entry_display.delete(0, tk.END)
         entry_display.insert(0, "Error")
 
+
 # Create the main window
 window = tk.Tk()
 window.title("Calculator")
 
+
 # Load the background image
 bg_image = PhotoImage(file="_calculator.png")
+
 
 # Get the screen width and height
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
+
 # the background image
 background_label = tk.Label(window, image=bg_image)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
 
 # label for the title
 title_label = tk.Label(window, text="Simple Calculator", font=("Arial", 18, "bold"), bg="white")
 title_label.place(relx=0.5, rely=0.05, anchor="n")
 
+
 # Entry widget for displaying the expression
 entry_display = tk.Entry(window, width=20, font=("Arial", 16))
 entry_display.place(relx=0.5, rely=0.3, anchor="n")
+
 
 # buttons for numbers and operations
 buttons = [
@@ -50,9 +61,11 @@ buttons = [
     '0', 'C', '=', '+'
 ]
 
+
 row, col = 0, 0
 button_frame = tk.Frame(window)
 button_frame.place(relx=0.5, rely=0.5, anchor="n")
+
 
 for button in buttons:
     if button == '=':
@@ -66,8 +79,10 @@ for button in buttons:
         col = 0
         row += 1
 
+
 # window size to cover the entire screen
 window.geometry(f"{screen_width}x{screen_height}")
+
 
 # GUI event loop
 window.mainloop()
